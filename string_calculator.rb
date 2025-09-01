92c487b8e7946194f08ecb77d7b44f29bfcd2497
Dexter1210 <deveshsharma1210@gmail.com>
2025-09-01 13:54:42 +0530

    handle the case for string containing multiple numbers seperated by commas

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,6 +5,12 @@ class StringCalculator
         # returns 0 if the input string is empty or no input passed
         return 0 if numbers.nil? || numbers == ""
         # return the number itself if only a single number
-        return numbers.to_i
+        return numbers.to_i unless numbers.include?(",")
+
+        # split the string by comma and sum the numbers
+        # @input is a string e.g. "1,2,3"
+        # split will split the strings based on , delimitator and then map will return an array of integers
+        # sum will return the sum of array elements
+        numbers.split(",").map(&:to_i).sum
     end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -14,4 +14,12 @@ describe StringCalculator do
     it 'returns the number itself for a single number input' do
         expect(calc.add("5")).to eq(5)
     end
+
+    it 'returns the sum of two numbers seperated by comma' do
+        expect(calc.add("1,2")).to eq(3)
+    end
+
+    it 'returns the sum of multiple numbers seperated by comma' do
+        expect(calc.add("1,2,3,4")).to eq(10)
+    end
 end
