3d2c2f3929467886538e68352639de697ac827f1
Devesh <Dexter1210>
2025-09-01 13:33:22 +0530

    add pull request template and add string calculator zero case implementation'

---
+++ string_calculator.rb
@@ -0,0 +1,8 @@
+# class decalartion for the String calculator
+class StringCalculator
+    # Adds numbers present in a string and returns the sum
+    def add(numbers)
+        # returns 0 if the input string is empty or no input passed
+        return 0 if numbers.nil? || numbers == ""
+    end
+end
